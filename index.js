e338124de219846cb7f5c7908b4ba37062dc9f3b
ansshour <andrey.zhukov001@gmail.com>
2022-05-31 02:15:46 +0300

    kfmfsdf

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import axios from "axios";
 
 const app = express()
 
 app.use(express.json());
 
 app.use(cors());
+
+const urlencodedParser = express.urlencoded({ extended: false });
 
 app.get("/getcomments", async (req, res) => {
     const offset = req.query.offset;
@@ -17,6 +20,24 @@ app.get("/getcomments", async (req, res) => {
     } catch (err) { console.log("error") }
 
 })
+
+
+app.post("/getcomments", urlencodedParser, (req, res) => {
+    console.log(req.body);
+    const token = req.body.dataForResponse.token;
+    const groupId = req.body.dataForResponse.groupid;
+    const topicId = req.body.dataForResponse.topicid;
+    const numbersOfComments = req.body.dataForResponse.numbersOfComments;
+    const offset = req.body.offset;
+
+
+    fetch(`https://api.vk.com/method/board.getComments?group_id=${groupId}&topic_id=${topicId}&count=100&extended=1&offset=${offset}&access_token=${token}&v=5.131`)
+        .then(response => response.json())
+        .then(data => res.send(data))
+        .catch(error => console.log(error))
+
+
+});
 
 
 app.listen(4000, () => {
